0d103840f45288332e204090b7ab424a93bbbd14
19PA1A05C3 <60594850+19PA1A05C3@users.noreply.github.com>
2022-07-21 09:14:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var bar = document.getElementById('bar');
-var nav = document.getElementById('navbar');
-
-if (bar){
-    bar.addEventListener('click',()=>{
-        nav.classList.add('active');
-    })
-}
